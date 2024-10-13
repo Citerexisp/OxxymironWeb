d9d9cf20722e6b61aeb4a78a0c9b5b0e1c1e35f3
Семен Хабаров <habarov.cemen@yandex.ru>
2024-10-14 01:17:30 +0300

    R

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// В этом простом примере мы кэшируем только главную страницу
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+    '/',
+    '/index.html'
+];
+
+// Устанавливаем service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Обрабатываем запросы
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Возвращаем ответ из кэша или загружаем из сети
+                return response || fetch(event.request);
+            })
+    );
+});
