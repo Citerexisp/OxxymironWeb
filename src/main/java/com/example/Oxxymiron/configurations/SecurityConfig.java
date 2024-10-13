b40002a2b8d261172057d7841dd5234aaaf130a7
Семен Хабаров <habarov.cemen@yandex.ru>
2024-10-14 00:41:06 +0300

    Иконки на рабочий стол

--- src/main/java/com/example/Oxxymiron/configurations/SecurityConfig.java
+++ src/main/java/com/example/Oxxymiron/configurations/SecurityConfig.java
@@ -1,35 +1,28 @@
-//package com.example.Oxxymiron.configurations;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/register", "/login", "/css/**", "/js/**").permitAll() // Разрешить доступ к этим страницам
-//                        .anyRequest().permitAll() // Все остальные страницы требуют аутентификации
-//                )
-//                .formLogin(form -> form
-//                        .loginPage("/login") // Страница входа
-//                        .permitAll() // Разрешить доступ к странице входа всем
-//                )
-//                .logout(logout -> logout
-//                        .permitAll() // Разрешить доступ к выходу всем
-//                );
-//
-//        return http.build();
-//    }
-//}
+package com.example.Oxxymiron.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authz -> authz
+                        .requestMatchers("/", "/index", "/oauth2/authorization/google").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2.permitAll())
+                .logout(logout -> logout
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutSuccessUrl("/")
+                );
+        return http.build();
+    }
+}
